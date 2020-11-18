f37f8d4e8f397b71092f4cc2e0fb16ac3bbccde3
Beatriz Milz <beatriz.milz@hotmail.com>
2020-11-18 11:00:14 -0300

    atualização materia/ anotacoes de aula

--- exemplos_de_aula/07-chancedegol_anotacoes_aula.R
+++ exemplos_de_aula/07-chancedegol_anotacoes_aula.R
@@ -6,12 +6,23 @@ u_cdg <- "http://www.chancedegol.com.br/br19.htm"
 
 cdg_html <- GET(u_cdg)
 
+
 cdg_table <- cdg_html  %>%
   read_html() %>%
   xml_find_first('//table') %>%
-  html_table()
+  html_table() %>%
+  tibble::as_tibble() %>%
+  set_names(janitor::make_clean_names(.[1,])) %>%
+  slice(-1)
+
 
 cores <- cdg_html %>%
   read_html() %>%
   xml_find_all('//font[@color="#FF0000"]') %>%
   xml_text()
+
+
+cdg_table$vermelho <- cores
+
+
+cdg_table
--- exercicios/06-html_resolucao.R
+++ exercicios/06-html_resolucao.R
@@ -5,13 +5,21 @@ html <- xml2::read_html("exemplos_de_aula/html_exemplo.html")
 
 # 1. Qual a diferença entre xml_find_all() e xml_find_first()?
 
-xml2::xml_find_all(html, "//p")
+xml2::xml_find_all(html, "//body")
 
-xml2::xml_find_first(html, "//p")
+xml2::xml_find_first(html, "//body")
+
+# EU TINHA ENTENDIDO ERRADO!
+# Minha resposta inicial foi:  xml_find_all retorna todas as ocorrências encontradas, e xml_find_first retorna apenas a primeira ocorrência.
+
+# Ver novamente o video onde o Julio explica. 19:57
 
 
-# xml_find_all retorna todas as ocorrências encontradas,
-# e xml_find_first retorna apenas a primeira ocorrência.
+# nova resposta com o que eu entendi
+# xml_find_all retorna uma lista com todas as ocorrências encontradas.
+# xml_find_first retorna o primeiro elemento, com uma lista com todas as tags filhas.
+
+
 
 
 # 2. O que faz a função contains() aplicada dentro do XPath?
@@ -23,3 +31,4 @@ xml_find_all(html, "//p[contains(@style, 'blue')]")
 # Funciona como um filtro nos atributos (nesse caso, filtrou os nodes em que
 # no atributo style tem a property blue)
 
+# Resposta Julio: similar ao stringr::str_detect(), mas para xpath
